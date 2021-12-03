87982735b60196b04f3a9e466ac6a09741413ed2
Anthony Krut <krut.anthony@gmail.com>
2021-12-03 18:32:59 +0200

    HomePage Quote refactored

---
+++ src/components/home-page/Quote.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="quote">
+    <div class="quote__text">
+      <p class="quote__text-paragraph">
+        В мире моды для нас нет границ и ограничений!
+      </p>
+      <p class="quote__text-paragraph">
+        BUMER следует самым актуальным модным трендам — в наших коллекциях
+        классические узоры сочетаются с оригинальными, ультрасовременными
+        решениями.
+      </p>
+    </div>
+    <div class="quote__author">
+      <div class="quote__logo">
+        <img
+          class="quote__logo-img"
+          alt="bumer logo"
+          src="../../assets/img/svg/logo-black.svg"
+        />
+      </div>
+      <div class="quote__date">EST. 1999</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quote',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variables";
+@import "../../assets/scss/functions";
+
+.quote {
+  width: 50%;
+  min-width: 300px;
+  margin: 0 auto 10em;
+  font-size: adaptive_fz(14px, 12px);
+  font-weight: 400;
+  text-align: center;
+  @media screen and (max-width: 991px) {
+    width: 75%;
+    margin-bottom: 2.5em;
+  }
+}
+
+.quote__text {
+  margin-bottom: 5em;
+  line-height: 1.85em;
+  color: $grey_color_dark;
+}
+
+.quote__text-paragraph {
+  &:first-child::before {
+    display: block;
+    width: 3.5em;
+    height: 3.5em;
+    margin: 0 auto 1.75em;
+    content: "";
+    background: url("../../assets/img/svg/quotes.svg") center/contain no-repeat;
+  }
+}
+
+.quote__author {
+  font-size: adaptive_fz(14px, 9px);
+  font-weight: 700;
+  color: $main_color;
+}
+
+.quote__logo {
+  width: 12.5vw;
+  min-width: 150px;
+  margin: 0 auto 1em;
+}
+
+.quote__logo-img {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+
+.quote__date {
+  text-transform: uppercase;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,224 +5,135 @@
     <div class="container">
       <Banner1 />
 
-      <div class="parallax-container">
-        <div class="popular-products">
-          <h3 class="popular-products__title">
-            ПОПУЛЯРНЫЕ ТОВАРЫ
-          </h3>
-          <div class="popular-products__line line line_fisrt">
-            <router-link
-              class="line__product product"
-              :to="{ name: 'Product' }"
-            >
-              <div class="product__label">
-                SALE
-              </div>
-              <div class="product__pic">
-                <img
-                  alt="product picture"
-                  src="../../public/img/stock-items/item01-01.png"
-                >
-              </div>
-              <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
-              </div>
-            </router-link>
-            <router-link
-              class="line__product product"
-              :to="{ name: 'Product' }"
-            >
-              <div class="product__label">
-                TOP
-              </div>
-              <div class="product__pic">
-                <img
-                  alt="product picture"
-                  src="../../public/img/stock-items/item02-01.png"
-                >
-              </div>
-              <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
-              </div>
-            </router-link>
-            <router-link
-              class="line__product product"
-              :to="{ name: 'Product' }"
-            >
-              <div class="product__label" />
-              <div class="product__pic">
-                <img
-                  alt="product picture"
-                  src="../../public/img/stock-items/item03-01.png"
-                >
-              </div>
-              <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
-              </div>
-            </router-link>
-            <router-link
-              class="line__product product"
-              :to="{ name: 'Product' }"
-            >
-              <div class="product__label">
-                NEW
-              </div>
-              <div class="product__pic">
-                <img
-                  alt="product picture"
-                  src="../../public/img/stock-items/item01-01.png"
-                >
-              </div>
-              <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
-              </div>
-            </router-link>
-          </div>
-
-          <Banner2 />
-
-          <div class="popular-products__line line line_second">
-            <div class="line__product product">
-              <div class="product__label">
-                SALE
-              </div>
-              <div class="product__pic">
-                <img
-                  alt="product picture"
-                  src="../../public/img/stock-items/item01-01.png"
-                >
-              </div>
-              <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
-              </div>
+      <div class="popular-products">
+        <h3 class="popular-products__title">ПОПУЛЯРНЫЕ ТОВАРЫ</h3>
+        <div class="popular-products__line line line_fisrt">
+          <router-link class="line__product product" :to="{ name: 'Product' }">
+            <div class="product__label">SALE</div>
+            <div class="product__pic">
+              <img
+                alt="product picture"
+                src="../../public/img/stock-items/item01-01.png"
+              />
             </div>
-            <div class="line__product product">
-              <div class="product__label">
-                TOP
-              </div>
-              <div class="product__pic">
-                <img
-                  alt="product picture"
-                  src="../../public/img/stock-items/item02-01.png"
-                >
-              </div>
-              <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
-              </div>
+            <div class="product__specs">
+              <div class="product__article">QN380</div>
+              <div class="product__price">2250 <span>грн</span></div>
             </div>
-            <div class="line__product product">
-              <div class="product__label" />
-              <div class="product__pic">
-                <img
-                  alt="product picture"
-                  src="../../public/img/stock-items/item03-01.png"
-                >
-              </div>
-              <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
-              </div>
+          </router-link>
+          <router-link class="line__product product" :to="{ name: 'Product' }">
+            <div class="product__label">TOP</div>
+            <div class="product__pic">
+              <img
+                alt="product picture"
+                src="../../public/img/stock-items/item02-01.png"
+              />
             </div>
-            <div class="line__product product">
-              <div class="product__label">
-                NEW
-              </div>
-              <div class="product__pic">
-                <img
-                  alt="product picture"
-                  src="../../public/img/stock-items/item01-01.png"
-                >
-              </div>
-              <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
-              </div>
+            <div class="product__specs">
+              <div class="product__article">QN380</div>
+              <div class="product__price">2250 <span>грн</span></div>
             </div>
-          </div>
+          </router-link>
+          <router-link class="line__product product" :to="{ name: 'Product' }">
+            <div class="product__label" />
+            <div class="product__pic">
+              <img
+                alt="product picture"
+                src="../../public/img/stock-items/item03-01.png"
+              />
+            </div>
+            <div class="product__specs">
+              <div class="product__article">QN380</div>
+              <div class="product__price">2250 <span>грн</span></div>
+            </div>
+          </router-link>
+          <router-link class="line__product product" :to="{ name: 'Product' }">
+            <div class="product__label">NEW</div>
+            <div class="product__pic">
+              <img
+                alt="product picture"
+                src="../../public/img/stock-items/item01-01.png"
+              />
+            </div>
+            <div class="product__specs">
+              <div class="product__article">QN380</div>
+              <div class="product__price">2250 <span>грн</span></div>
+            </div>
+          </router-link>
         </div>
 
-        <div class="parallax-container">
-          <Story1 />
+        <Banner2 />
 
-          <div class="parallax-container">
-            <div class="parallax-wrapper">
-              <Story2 />
+        <div class="popular-products__line line line_second">
+          <div class="line__product product">
+            <div class="product__label">SALE</div>
+            <div class="product__pic">
+              <img
+                alt="product picture"
+                src="../../public/img/stock-items/item01-01.png"
+              />
             </div>
-
-            <div class="parallax-container">
-              <div class="map">
-                <div class="map__numbers">
-                  <span>10+</span>
-                  <span>магазинов по Украине</span>
-                </div>
-                <div class="map__pic">
-                  <img alt="map of bumer stores" src="../assets/img/map.svg">
-                </div>
-              </div>
-
-              <div class="quote">
-                <div class="quote__text">
-                  <p>В мире моды для нас нет границ и ограничений!</p>
-                  <p>
-                    BUMER следует самым актуальным модным трендам — в наших
-                    коллекциях классические узоры сочетаются с оригинальными,
-                    ультрасовременными решениями.
-                  </p>
-                </div>
-                <div class="quote__author author">
-                  <div class="author__logo">
-                    <img
-                      alt="bumer logo"
-                      src="../assets/img/svg/logo-black.svg"
-                    >
-                  </div>
-                  <div class="author__date">
-                    EST. 1999
-                  </div>
-                </div>
-              </div>
+            <div class="product__specs">
+              <div class="product__article">QN380</div>
+              <div class="product__price">2250 <span>грн</span></div>
+            </div>
+          </div>
+          <div class="line__product product">
+            <div class="product__label">TOP</div>
+            <div class="product__pic">
+              <img
+                alt="product picture"
+                src="../../public/img/stock-items/item02-01.png"
+              />
+            </div>
+            <div class="product__specs">
+              <div class="product__article">QN380</div>
+              <div class="product__price">2250 <span>грн</span></div>
+            </div>
+          </div>
+          <div class="line__product product">
+            <div class="product__label" />
+            <div class="product__pic">
+              <img
+                alt="product picture"
+                src="../../public/img/stock-items/item03-01.png"
+              />
+            </div>
+            <div class="product__specs">
+              <div class="product__article">QN380</div>
+              <div class="product__price">2250 <span>грн</span></div>
+            </div>
+          </div>
+          <div class="line__product product">
+            <div class="product__label">NEW</div>
+            <div class="product__pic">
+              <img
+                alt="product picture"
+                src="../../public/img/stock-items/item01-01.png"
+              />
+            </div>
+            <div class="product__specs">
+              <div class="product__article">QN380</div>
+              <div class="product__price">2250 <span>грн</span></div>
             </div>
           </div>
         </div>
       </div>
+
+      <Story1 />
+
+      <Story2 />
+
+      <div class="map">
+        <div class="map__numbers">
+          <span>10+</span>
+          <span>магазинов по Украине</span>
+        </div>
+        <div class="map__pic">
+          <img alt="map of bumer stores" src="../assets/img/map.svg" />
+        </div>
+      </div>
+      <Quote />
     </div>
   </section>
 </template>
@@ -233,10 +144,12 @@ import Banner1 from '@/components/home-page/Banner1';
 import Banner2 from '@/components/home-page/Banner2';
 import Story2 from '@/components/home-page/Story2';
 import Story1 from '@/components/home-page/Story1';
+import Quote from '@/components/home-page/Quote';
 
 export default {
   name: 'HomePage',
   components: {
+    Quote,
     Story2,
     Story1,
     Banner2,
@@ -309,50 +222,6 @@ export default {
           display: none;
         }
       }
-    }
-  }
-  .quote {
-    width: 50%;
-    min-width: 300px;
-    margin: 0 auto 6em;
-    font-size: adaptive_fz(20px, 12px);
-    font-weight: 400;
-    text-align: center;
-    &__text {
-      margin-bottom: 3.5em;
-      line-height: 1.5em;
-      color: $grey_color_dark;
-      p:first-child::before {
-        display: block;
-        width: 2.5em;
-        height: 2.5em;
-        margin: 0 auto 1.25em;
-        content: "";
-        background: url("../assets/img/svg/quotes.svg") center/contain no-repeat;
-      }
-    }
-    &__author {
-      font-size: adaptive_fz(14px, 9px);
-      font-weight: 700;
-      color: $main_color;
-
-      .author__logo {
-        width: 12.5vw;
-        min-width: 150px;
-        margin: 0 auto 1em;
-        img {
-          width: 100%;
-          height: 100%;
-          display: block;
-        }
-      }
-      .author__date {
-        text-transform: uppercase;
-      }
-    }
-    @media screen and (max-width: 991px) {
-      width: 75%;
-      margin-bottom: 3.5em;
     }
   }
 }
