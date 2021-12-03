7d7a54b399f9974eae6b9ef4dc6d6fdf1b44d13a
Anthony Krut <krut.anthony@gmail.com>
2021-12-03 17:26:46 +0200

    HomePage Map refactored

---
+++ src/components/home-page/Map.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="map">
+    <div class="map__numbers">
+      <span class="map__digit">10+</span>
+      <span class="map__text">магазинов по Украине</span>
+    </div>
+    <div class="map__pic">
+      <img
+        class="map__pic-img"
+        alt="map of bumer stores"
+        src="../../assets/img/map.svg"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variables";
+@import "../../assets/scss/functions";
+@import "../../assets/scss/mixins";
+
+.map {
+  position: relative;
+  margin-bottom: 1em;
+  font-size: adaptive_fz(70px, 20px);
+  font-weight: 600;
+  @media screen and (min-width: 1440px) {
+    font-size: 70px;
+  }
+}
+
+.map__numbers {
+  position: absolute;
+  top: calc(0% - 0.33 * (2 * 3vw + 1.15 * adaptive_fz(70px, 20px)));
+  left: 3vw;
+  display: flex;
+  align-items: center;
+  width: 40%;
+  padding: Min(3vw, 60px) Min(4vw, 60px);
+  color: $contrast_color;
+  background-color: $main_color;
+  @media screen and (max-width: 575px) {
+    display: none;
+  }
+}
+
+.map__digit {
+  margin-right: 4vw;
+}
+
+.map__text {
+  font-size: adaptive_fz(14px, 8px);
+  font-weight: 400;
+  color: $grey_color;
+  @media screen and (min-width: 1440px) {
+    font-size: 14px;
+  }
+}
+
+.map__pic {
+  @media screen and (max-width: 575px) {
+    height: 13em;
+    background: url("../../assets/img/map.svg") 70% 40%/275% no-repeat;
+  }
+}
+
+.map__pic-img {
+  display: block;
+  width: 100%;
+  @media screen and (max-width: 575px) {
+    display: none;
+  }
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,52 +7,38 @@
 
       <div class="parallax-container">
         <div class="popular-products">
-          <h3 class="popular-products__title">
-            ПОПУЛЯРНЫЕ ТОВАРЫ
-          </h3>
+          <h3 class="popular-products__title">ПОПУЛЯРНЫЕ ТОВАРЫ</h3>
           <div class="popular-products__line line line_fisrt">
             <router-link
               class="line__product product"
               :to="{ name: 'Product' }"
             >
-              <div class="product__label">
-                SALE
-              </div>
+              <div class="product__label">SALE</div>
               <div class="product__pic">
                 <img
                   alt="product picture"
                   src="../../public/img/stock-items/item01-01.png"
-                >
+                />
               </div>
               <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
+                <div class="product__article">QN380</div>
+                <div class="product__price">2250 <span>грн</span></div>
               </div>
             </router-link>
             <router-link
               class="line__product product"
               :to="{ name: 'Product' }"
             >
-              <div class="product__label">
-                TOP
-              </div>
+              <div class="product__label">TOP</div>
               <div class="product__pic">
                 <img
                   alt="product picture"
                   src="../../public/img/stock-items/item02-01.png"
-                >
+                />
               </div>
               <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
+                <div class="product__article">QN380</div>
+                <div class="product__price">2250 <span>грн</span></div>
               </div>
             </router-link>
             <router-link
@@ -64,37 +50,27 @@
                 <img
                   alt="product picture"
                   src="../../public/img/stock-items/item03-01.png"
-                >
+                />
               </div>
               <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
+                <div class="product__article">QN380</div>
+                <div class="product__price">2250 <span>грн</span></div>
               </div>
             </router-link>
             <router-link
               class="line__product product"
               :to="{ name: 'Product' }"
             >
-              <div class="product__label">
-                NEW
-              </div>
+              <div class="product__label">NEW</div>
               <div class="product__pic">
                 <img
                   alt="product picture"
                   src="../../public/img/stock-items/item01-01.png"
-                >
+                />
               </div>
               <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
+                <div class="product__article">QN380</div>
+                <div class="product__price">2250 <span>грн</span></div>
               </div>
             </router-link>
           </div>
@@ -103,41 +79,29 @@
 
           <div class="popular-products__line line line_second">
             <div class="line__product product">
-              <div class="product__label">
-                SALE
-              </div>
+              <div class="product__label">SALE</div>
               <div class="product__pic">
                 <img
                   alt="product picture"
                   src="../../public/img/stock-items/item01-01.png"
-                >
+                />
               </div>
               <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
+                <div class="product__article">QN380</div>
+                <div class="product__price">2250 <span>грн</span></div>
               </div>
             </div>
             <div class="line__product product">
-              <div class="product__label">
-                TOP
-              </div>
+              <div class="product__label">TOP</div>
               <div class="product__pic">
                 <img
                   alt="product picture"
                   src="../../public/img/stock-items/item02-01.png"
-                >
+                />
               </div>
               <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
+                <div class="product__article">QN380</div>
+                <div class="product__price">2250 <span>грн</span></div>
               </div>
             </div>
             <div class="line__product product">
@@ -146,34 +110,24 @@
                 <img
                   alt="product picture"
                   src="../../public/img/stock-items/item03-01.png"
-                >
+                />
               </div>
               <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
+                <div class="product__article">QN380</div>
+                <div class="product__price">2250 <span>грн</span></div>
               </div>
             </div>
             <div class="line__product product">
-              <div class="product__label">
-                NEW
-              </div>
+              <div class="product__label">NEW</div>
               <div class="product__pic">
                 <img
                   alt="product picture"
                   src="../../public/img/stock-items/item01-01.png"
-                >
+                />
               </div>
               <div class="product__specs">
-                <div class="product__article">
-                  QN380
-                </div>
-                <div class="product__price">
-                  2250 <span>грн</span>
-                </div>
+                <div class="product__article">QN380</div>
+                <div class="product__price">2250 <span>грн</span></div>
               </div>
             </div>
           </div>
@@ -188,15 +142,7 @@
             </div>
 
             <div class="parallax-container">
-              <div class="map">
-                <div class="map__numbers">
-                  <span>10+</span>
-                  <span>магазинов по Украине</span>
-                </div>
-                <div class="map__pic">
-                  <img alt="map of bumer stores" src="../assets/img/map.svg">
-                </div>
-              </div>
+              <Map />
 
               <div class="quote">
                 <div class="quote__text">
@@ -212,11 +158,9 @@
                     <img
                       alt="bumer logo"
                       src="../assets/img/svg/logo-black.svg"
-                    >
+                    />
                   </div>
-                  <div class="author__date">
-                    EST. 1999
-                  </div>
+                  <div class="author__date">EST. 1999</div>
                 </div>
               </div>
             </div>
@@ -233,10 +177,12 @@ import Banner1 from '@/components/home-page/Banner1';
 import Banner2 from '@/components/home-page/Banner2';
 import Story2 from '@/components/home-page/Story2';
 import Story1 from '@/components/home-page/Story1';
+import Map from '@/components/home-page/Map';
 
 export default {
   name: 'HomePage',
   components: {
+    Map,
     Story2,
     Story1,
     Banner2,
@@ -261,55 +207,6 @@ export default {
 
   .popular-products {
     @include popular-products;
-  }
-
-  .map {
-    position: relative;
-    margin-bottom: 1em;
-    font-size: adaptive_fz(70px, 20px);
-    font-weight: 600;
-    &__numbers {
-      position: absolute;
-      top: calc(0% - 0.33 * (2 * 3vw + 1.15 * adaptive_fz(70px, 20px)));
-      left: 3vw;
-      display: flex;
-      align-items: center;
-      width: 40%;
-      padding: 3vw 4vw;
-      color: $contrast_color;
-      background-color: $main_color;
-      span {
-        &:first-child {
-          margin-right: 4vw;
-        }
-        &:last-child {
-          font-size: adaptive_fz(14px, 8px);
-          font-weight: 400;
-          color: $grey_color;
-        }
-      }
-    }
-    &__pic {
-      img {
-        display: block;
-        width: 100%;
-      }
-    }
-    @media screen and (min-width: 1440px) {
-      font-size: 70px;
-    }
-    @media screen and (max-width: 575px) {
-      &__numbers {
-        display: none;
-      }
-      &__pic {
-        height: 13em;
-        background: url("../assets/img/map.svg") 70% 40%/275% no-repeat;
-        img {
-          display: none;
-        }
-      }
-    }
   }
   .quote {
     width: 50%;
