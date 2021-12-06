003e1ed4380932272fd9b17f63b5afae776496c0
Anthony Krut <krut.anthony@gmail.com>
2021-12-06 12:41:14 +0200

    ShipmentPage draft added

---
+++ src/components/common/TermsNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="terms-nav">
+    <router-link class="terms-nav__item" :to="{ name: '' }">
+      Доставка
+    </router-link>
+    <router-link class="terms-nav__item" :to="{ name: '' }">
+      Оплата
+    </router-link>
+    <router-link class="terms-nav__item" :to="{ name: '' }">
+      Гарантия
+    </router-link>
+    <router-link class="terms-nav__item" :to="{ name: '' }">
+      Сотрудничество
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TermsNav',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variables";
+</style>
