2de78a2ea218655575f891b067ee76fb3e9039d4
Anthony Krut <krut.anthony@gmail.com>
2022-01-13 13:51:03 +0200

    Privacy policy and Public offer pages redone

--- src/pages/PrivacyPolicyInfoPage.vue
+++ src/pages/PrivacyPolicyInfoPage.vue
@@ -1,64 +1,11 @@
 <template>
   <section class="privacy-policy">
     <div class="container">
-      <h1>
+      <h1 class="privacy-policy__title">
         {{ $t('common.privacy_policy') }}
       </h1>
       <div class="redactor-content">
-        <article>
-          <h4>
-            {{ $t('privacy.subtitle_1') }}
-          </h4>
-          <p>
-            {{ $t('privacy.text_1_1') }}
-            <a class="privacy-policy__link" :href="eu_url">{{ $t('privacy.text_1_link') }}</a>
-            {{ $t('privacy.text_1_2') }}
-          </p>
-          <h4>
-            {{ $t('privacy.subtitle_2') }}
-          </h4>
-          <p>
-            {{ $t('privacy.text_2') }}
-          </p>
-          <h4>
-            {{ $t('privacy.subtitle_3') }}
-          </h4>
-          <p>
-            {{ $t('privacy.text_3') }}
-          </p>
-          <h4>
-            {{ $t('privacy.subtitle_4') }}
-          </h4>
-          <p>
-            {{ $t('privacy.text_4') }}
-          </p>
-          <h4>
-            {{ $t('privacy.subtitle_5') }}
-          </h4>
-          <p>
-            {{ $t('privacy.text_5_1') }}
-            <a class="privacy-policy__link" :href="wiki_url">{{ $t('privacy.text_5_link') }}</a>
-            {{ $t('privacy.text_5_2') }}
-          </p>
-          <h4>
-            {{ $t('privacy.subtitle_6') }}
-          </h4>
-          <p>
-            {{ $t('privacy.text_6') }}
-          </p>
-          <h4>
-            {{ $t('privacy.subtitle_7') }}
-          </h4>
-          <p>
-            {{ $t('privacy.text_7') }}
-          </p>
-          <h4>
-            {{ $t('privacy.subtitle_8') }}
-          </h4>
-          <p>
-            {{ $t('privacy.text_8') }}
-          </p>
-        </article>
+        <article v-html="$t('privacy.text')" />
       </div>
     </div>
   </section>
@@ -67,40 +14,16 @@
 <script>
 export default {
   name: 'PrivacyPolicyInfoPage',
-  data() {
-    return {
-      eu_url:'https://ec.europa.eu/info/law/law-topic/data-protection_en',
-      wiki_url_ru: 'https://ru.wikipedia.org/wiki/Cookie',
-      wiki_url_ua: 'https://uk.wikipedia.org/wiki/Куки', 
-    }
-  },
-  computed: {
-    wiki_url() {
-      let url = ''
-      if (this.$i18n.locale === 'ru') {
-          url = this.wiki_url_ru
-        } else if (this.$i18n.locale === 'ua') {
-         url = this.wiki_url_ua
-        }
-      return url
-    },
-  },
 }
 </script>
 
 <style lang="scss" scoped>
 
-.redactor-content {
-  justify-content: center;
+.privacy-policy__title {
+  margin-bottom: 1.5em;
 }
 
-.privacy-policy__link {
-  color: #2048bf;
-  text-decoration: none;
-
-  &:hover {
-    color: #2048bf;
-    text-decoration: underline;
-  }
+.redactor-content {
+  justify-content: center;
 }
 </style>
